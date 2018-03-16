fbcf080b278c3fc2883e16cb4127ea41f63bfe6a
Роман <roma-177@mail.ru>
2018-03-16 17:52:22 +0300

    Methods modifidet with bitwise operations

--- src/main/java/bitset/BitSet.java
+++ src/main/java/bitset/BitSet.java
@@ -10,92 +10,149 @@ import java.util.Collections;
 import java.util.List;
 
 public class BitSet {
-    private List<Integer> bits = new ArrayList<Integer>();
+    private final static int ADDRESS_BITS_PER_WORD = 6;
+    private final static int BITS_PER_WORD = 1;
+    private final static int BIT_INDEX_MASK = BITS_PER_WORD - 1;
+    private static final long WORD_MASK = 0xffffffffffffffffL;
 
-    BitSet(List<Integer> elements) {
-        for (int element : elements) {
-            if (element >= 0) {
-                if (!contains(element)) {
-                    bits.add(element);
-                }
+    private long[] words;
+    private transient int wordsInUse = 0;
+
+    private static int wordIndex(int bitIndex) {
+        return bitIndex >> ADDRESS_BITS_PER_WORD;
+    }
+
+    private void initWords(int nbits) {
+        words = new long[wordIndex(nbits - 1) + 1];
+    }
+
+    public BitSet() {
+        initWords(BITS_PER_WORD);
+    }
+
+    public BitSet(int nbits) {
+        if (nbits < 0) {
+            throw new NegativeArraySizeException();
+        }
+        initWords(nbits);
+    }
+
+    private BitSet(long[] words) {
+        this.words = words;
+        this.wordsInUse = words.length;
+    }
+
+    private void isInRange(int index) {
+        if (index < 0 || wordIndex(index) > wordsInUse) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void isInRange(int fromIndex, int toIndex) {
+        if (fromIndex < 0 || toIndex < fromIndex || wordIndex(toIndex) > wordsInUse) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void addElement(int bitIndex) {
+        isInRange(bitIndex);
+        int wordIndex = wordIndex(bitIndex);
+        words[wordIndex] |= (1L << bitIndex);
+    }
+
+    public void addMultiplicity(int fromIndex, int toIndex) {
+        isInRange(fromIndex, toIndex);
+        int startWordIndex = wordIndex(fromIndex);
+        int endWordIndex = wordIndex(toIndex - 1);
+        long firstWordMask = WORD_MASK << fromIndex;
+        long lastWordMask = WORD_MASK >>> -toIndex;
+        if (startWordIndex == endWordIndex) {
+            words[startWordIndex] |= (firstWordMask & lastWordMask);
+        } else {
+            words[startWordIndex] |= firstWordMask;
+            for (int i = startWordIndex + 1; i < lastWordMask; i++) {
+                words[i] = WORD_MASK;
             }
+            words[endWordIndex] = lastWordMask;
         }
     }
 
-    public int getSize() {
-        return bits.size();
+    public void deleteElement(int bitIndex) {
+        isInRange(bitIndex);
+        int wordIndex = wordIndex(bitIndex);
+        words[wordIndex] &= ~(1L << bitIndex);
     }
 
-    public List<Integer> getBits() {
-        return bits;
-    }
-
-    public void addElement(int element) {
-        if (element >= 0) {
-            if (!contains(element)) {
-                bits.add(element);
+    public void deleteMultiplicity(int fromIndex, int toIndex) {
+        isInRange(fromIndex, toIndex);
+        int startWordIndex = wordIndex(fromIndex);
+        int endWordIndex = wordIndex(toIndex - 1);
+        long firstWordMask = WORD_MASK << fromIndex;
+        long lastWordMask = WORD_MASK >>> -toIndex;
+        if (startWordIndex == endWordIndex) {
+            words[startWordIndex] &= ~(firstWordMask & lastWordMask);
+        } else {
+            words[startWordIndex] &= ~firstWordMask;
+            for (int i = startWordIndex + 1; i < lastWordMask; i++) {
+                words[i] = 0;
             }
+            words[endWordIndex] &= ~lastWordMask;
         }
     }
 
-    public void addMultiplicity(List<Integer> elements) {
-        for (int element : elements) {
-            addElement(element);
-        }
-    }
-
-    public void deleteElement(int element) {
-        if (element >= 0) {
-            if (contains(element)) {
-                bits.remove((Object) element);
-            }
-        }
-    }
-
-    public void deleteMultiplicity(List<Integer> elements) {
-        for (int element : elements) {
-            deleteElement(element);
-        }
-    }
-
-    public boolean contains(int element) {
-        if (element >= 0) {
-            if (bits.contains(element)) {
-                return true;
-            }
+    public boolean contains(int bitIndex) {
+        isInRange(bitIndex);
+        int wordIndex = wordIndex(bitIndex);
+        if (words[wordIndex] == (words[wordIndex] | ~(1L << bitIndex))) {
+            return true;
         }
         return false;
     }
 
     public BitSet crossing(BitSet other) {
-        BitSet crossingBitSet = new BitSet(Collections.<Integer>emptyList());
-        for (int elemnt : this.getBits()) {
-            if (this.contains(elemnt) && other.contains(elemnt)) {
-                crossingBitSet.addElement(elemnt);
-            }
+        if (this == other) {
+            return this;
         }
-        return crossingBitSet;
+        int wordsInCommon = Math.min(wordsInUse, other.wordsInUse);
+        BitSet mainCrossing;
+        BitSet assistingCrossing;
+        if (wordsInCommon == wordsInUse) {
+            mainCrossing = this;
+            assistingCrossing = other;
+        } else {
+            mainCrossing = other;
+            assistingCrossing = this;
+        }
+        for (int i = 0; i < wordsInCommon; i++) {
+            mainCrossing.words[i] &= assistingCrossing.words[i];
+        }
+        return mainCrossing;
     }
 
     public BitSet union(BitSet other) {
-        BitSet unionBitSet = new BitSet(Collections.<Integer>emptyList());
-        unionBitSet.addMultiplicity(this.getBits());
-        unionBitSet.addMultiplicity(other.getBits());
-        return unionBitSet;
+        if (this == other) {
+            return this;
+        }
+        int wordsInCommon = Math.min(wordsInUse, other.wordsInUse);
+        BitSet assistingUnion;
+        BitSet mainUnion;
+        if (wordsInCommon == wordsInUse) {
+            assistingUnion = this;
+            mainUnion = other;
+        } else {
+            assistingUnion = other;
+            mainUnion = this;
+        }
+        for (int i = 0; i < wordsInCommon; i++) {
+            mainUnion.words[i] |= assistingUnion.words[i];
+        }
+        return mainUnion;
     }
 
     public BitSet addition() {
-        BitSet addtionBitSet = new BitSet(Collections.<Integer>emptyList());
-        int max = 0;
-        for (int element : bits) {
-            if (element > max) {
-                max = element;
-            }
-        }
-        for (; max >= 0; max--) {
-            if (!contains(max)) {
-                addtionBitSet.addElement(max);
-            }
+        BitSet addtionBitSet = this;
+        for (int i = 0; i < wordsInUse; i++) {
+            words[i] = ~words[i];
         }
         return addtionBitSet;
     }
@@ -107,14 +164,14 @@ public class BitSet {
             return true;
         }
         BitSet other = (BitSet) obj;
-        if (this.getSize() == other.getSize()) {
-            for (int element : this.getBits()) {
-                if (!other.contains(element)) {
-                    return false;
-                }
-            }
-            return true;
+        if (wordsInUse != other.wordsInUse) {
+            return false;
         }
-        return false;
+        for (int i = 0; i < wordsInUse; i++) {
+            if (words[i] != other.words[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
